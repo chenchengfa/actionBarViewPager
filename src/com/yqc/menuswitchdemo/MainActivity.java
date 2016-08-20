ae07b25092f81599c2d8eb44913dd948af501b6f
chencf <330587818@qq.com>
2016-08-20 15:08:54 +0800

    new branch1

--- src/com/yqc/menuswitchdemo/MainActivity.java
+++ src/com/yqc/menuswitchdemo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.yqc.menuswitchdemo;
 
-import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
